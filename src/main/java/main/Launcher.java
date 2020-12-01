39bb51af396995e10164f518ddf987cc92deda9a
unknown <henrique.barcia@gec.inatel.br>
2020-12-01 20:46:22 -0300

    Fix indentation

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -3,7 +3,7 @@ package main;
 import javafx.application.Application;
 
 public class Launcher {
-	public static void main(String[] args){
-        Application.launch(Runnable.class, args);
-    }
+	public static void main(String[] args) {
+		Application.launch(Runnable.class, args);
+	}
 }
